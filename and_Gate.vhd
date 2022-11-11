304ae4ec016bd46ccdcd7ecdac4bba2b3a4b6381
Milind <milind.official98@gmail.com>
2022-11-11 11:23:45 +0100

    Build entity with 2 inputs and 1 output for and gate

---
+++ and_Gate.vhd
@@ -0,0 +1,14 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+-- Defining entities. Like a table of contents
+entity example_and is
+    port (
+        input_1 : in std_logic;
+        input_2 : in std_logic;
+        and_result : out std_logic
+        
+    );
+end example_and;
+
+
