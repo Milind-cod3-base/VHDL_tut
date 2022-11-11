d3630d4cd3465cc6c0cedd81b2b5cad1921add59
Milind <milind.official98@gmail.com>
2022-11-11 11:31:37 +0100

    Build architecture and assign inputs and outputs

--- and_Gate.vhd
+++ and_Gate.vhd
@@ -3,12 +3,20 @@ use ieee.std_logic_1164.all;
 
 -- Defining entities. Like a table of contents
 entity example_and is
-    port (
-        input_1 : in std_logic;
-        input_2 : in std_logic;
-        and_result : out std_logic
-        
-    );
+  port (
+    input_1 : in std_logic;
+    input_2 : in std_logic;
+    and_result : out std_logic
+
+  );
 end example_and;
 
+-- Defining Architecture
+architecture rtl of example_and is
+  signal and_gate : std_logic; -- Create a signal with name and gate
 
+begin
+  and_gate <= input_1 and input_2; -- Assigning inputs
+  and_result <= and_gate; -- Assignnig outputs
+
+end rtl;
