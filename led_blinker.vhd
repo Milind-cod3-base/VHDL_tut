36a64b55ff880ddee6b4c21ee8705eb7318b1eb9
Milind <milind.official98@gmail.com>
2022-11-14 06:45:19 +0100

    Add architecture: define constants

--- led_blinker.vhd
+++ led_blinker.vhd
@@ -14,6 +14,23 @@ entity led_blink is port(
 
 end led_blink;
 
+-- defining architecture
+architecture rtl of led_blink is
+
+    -- Constants to create the frequencies needed
+    -- formula is: (25 MHz / 100 Hz * 50% duty cycle)
+    -- So for 100 Hz: 25,000,000 / 100 * 0.5 = 125,000
+    constant c_CNT_100Hz : natural := 125000;
+    constant c_CNT_50Hz : natural := 250000;
+    constant c_CNT_10Hz : natural := 1250000;
+    constant c_CNT_1Hz : natural := 12500000;
+
+    -- These signals will be the counters:
+    -- For the design there are four counter processes that run concurrently. 
+    -- This means that they are all running at the exact same time. T
+    -- Their job is to keep track of the number of clock pulses seen for each of the different frequencies. 
+    -- Even if the switches are not selecting that particular frequency, the counters are still running!
+    signal r_CNT_100Hz : natural range 0 to c_CNT_100Hz;
 
     
 
